a12271d0ec000953a8985e99edc91578bafd9252
vinayksj <115333707+vinayksj@users.noreply.github.com>
2026-07-31 18:25:23 +0530

    commit

---
+++ vowels.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		char ch=s.next().charAt(0);
+		int arr[]={'a','e','i','o','u'};
+		boolean isvowel=false;
+		for (int i=0;i<arr.length;i++)
+		{
+		    if(ch==arr[i])
+		    {
+		        isvowel=true;
+		        break;
+		       
+		    }
+		}
+		if(isvowel)
+		{
+		    System.out.println("is vowel");
+		}
+		else{
+		    System.out.println("is consonant");
+		}
+		  
+	
+		
+		
+		    
+		
+		
+	}
+	
+}
