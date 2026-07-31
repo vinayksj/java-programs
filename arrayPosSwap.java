f69a2d2626ccb851fca07f05b27d8340234d8e81
vinayksj <115333707+vinayksj@users.noreply.github.com>
2026-07-31 20:36:34 +0530

    Implement array position swap functionality

---
+++ arrayPosSwap.java
@@ -0,0 +1,17 @@
+class Main{
+    public static void main(String args[])
+    {
+        int arr[]={1,2,3,4,5};
+        System.out.println("array swap "+swap(arr,1,3));
+        for(int ele:arr)
+        System.out.println(ele);
+       
+    }    
+    public static int[] swap(int a[],int pos1,int pos2){
+        int temp=a[pos1];
+        a[pos1]=a[pos2];
+        a[pos2]=temp;
+        return a;
+    }
+    
+}
