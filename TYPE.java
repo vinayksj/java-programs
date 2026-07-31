7aa07da9715ca2d537dc44fcb2d56deecf5f0602
vinayksj <115333707+vinayksj@users.noreply.github.com>
2026-07-31 18:59:11 +0530

    commit

---
+++ TYPE.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		char ch=s.next().charAt(0);
+		if((ch>='A' && ch<='Z') ||(ch>='a' && ch<='z'))
+		{
+		    System.out.println(" it is a character");
+		}
+		else if(ch>='0' && ch<='9')
+		System.out.println("it is number");
+		else
+		System.out.println("it is a special char");
+		
+	}
+}
