330a3e545fda4acb8012f73b8bbe97a2c60de9c9
vinayksj <115333707+vinayksj@users.noreply.github.com>
2026-07-31 18:26:38 +0530

    commit
    Refactor main method formatting and remove unnecessary whitespace.

--- vowels.java
+++ vowels.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class Main
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Scanner s=new Scanner(System.in);
 		char ch=s.next().charAt(0);
 		int arr[]={'a','e','i','o','u'};
@@ -22,13 +23,6 @@ public class Main
 		else{
 		    System.out.println("is consonant");
 		}
-		  
-	
-		
-		
-		    
-		
-		
 	}
 	
 }
